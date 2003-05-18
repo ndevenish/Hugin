895e8175d321d1223bb968f883a5e41f97b0f19a
dangelo <dangelo@users.sourceforge.net>
2003-05-18 15:57:53 +0000

    added initial wxwindows port of the CPEditor and CPImageCtrl widget

---
+++ src/wxwin/ImgScrollWindow.h
@@ -0,0 +1,67 @@
+// -*- c-basic-offset: 4 -*-
+/** @file ImgScrollWindow.h
+ *
+ *  @author Pablo d'Angelo <pablo.dangelo@web.de>
+ *
+ *  $Id$
+ *
+ *  This is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This software is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public
+ *  License along with this software; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#ifndef _IMGSCROLLWINDOW_H
+#define _IMGSCROLLWINDOW_H
+
+#include "wx/wxprec.h"
+
+#ifndef WX_PRECOMP
+#include "wx/wx.h"
+#endif
+
+
+/** brief description.
+ *
+ *  What this does
+ */
+class ImgScrollWindow : public wxScrolledWindow
+{
+public:
+
+  ImgScrollWindow();
+    /** ctor.
+     */
+    ImgScrollWindow(wxWindow* parent);
+
+    /** dtor.
+     */
+    virtual ~ImgScrollWindow();
+
+  /// its current image
+  void setImage (const wxImage & img);
+
+protected:
+  void OnPaint(wxPaintEvent& event);
+
+private:
+
+  wxBitmap *bitmap;
+  
+  DECLARE_CLASS(ImgScrollWindow)
+  DECLARE_EVENT_TABLE()
+};
+
+
+
+#endif // _IMGSCROLLWINDOW_H
