6bbbc9c9166bf527701af5c94eba7755713d05df
dangelo <dangelo@users.sourceforge.net>
2004-02-18 07:16:52 +0000

    added general wx includes (needed for compilation under mingw)

--- src/hugin/MyProgressDialog.cpp
+++ src/hugin/MyProgressDialog.cpp
@@ -24,6 +24,14 @@
  *
  */
 
+#include <wx/wxprec.h>
+#ifdef __BORLANDC__
+    #pragma hdrstop
+#endif
+
+#ifndef WX_PRECOMP
+    #include <wx/wx.h>
+#endif
 
 #include "hugin/MyProgressDialog.h"
 #include "PT/PTOptimise.h"
