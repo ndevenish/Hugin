fe88ae81342c0e275e1f2a873dea4cbef31e8723
dangelo <dangelo@users.sourceforge.net>
2004-11-01 18:04:34 +0000

    allow build with gcc

--- src/include/vigra_ext/tiffUtils.h
+++ src/include/vigra_ext/tiffUtils.h
@@ -153,7 +153,7 @@ createRGBATiffImage(ImageIterator upperleft, ImageIterator lowerright,
                     vigra::TiffImage * tiff, int sampleformat)
 {
     typedef typename ImageAccessor::value_type PType;
-    typedef PType::value_type PixelType;
+    typedef typename PType::value_type PixelType;
 //    typedef typename ImageAccessor::value_type PixelType;
     int w = lowerright.x - upperleft.x;
     int h = lowerright.y - upperleft.y;
