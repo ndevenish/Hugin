46973183a62c5b14e470a98f1075233fd2a659f4
dangelo <dangelo@users.sourceforge.net>
2005-05-16 19:47:17 +0000

    updated build script for wxwidgets 2.6

--- utils/build_wxwin_msvc.bat
+++ utils/build_wxwin_msvc.bat
@@ -1,16 +1,9 @@
 rem build wxwindows
 rem
 
-echo "In order to use the msvc project files, download wxWidgets 2.6,"
-echo "and create this source tree:"
-echo ""
-echo "hugin              <- hugin source code"
-echo "libs/wxWidgets     <- wxWidgets 2.6 source (rename wxWidgets-2.6.0 to wxWidgets)"
-echo "libs/boost_1_31_0  <- boost library, only the headers are needed"
-echo "libs/libpano       <- pano12.dll"
-echo "Press return to continue"
-
-pause
+@echo "In order to use the msvc project files, please read",
+@echo "windows_DOT_NET_compile.txt"
+@echo ""
 
 copy ../src/include/config_msvc.h ../src/include/config.h
 
@@ -20,3 +13,4 @@ nmake -f makefile.vc BUILD=debug UNICODE=0 SHARED=0 RUNTIME_LIBS=static
 nmake -f makefile.vc BUILD=release UNICODE=0 SHARED=0 RUNTIME_LIBS=static
 nmake -f makefile.vc BUILD=release UNICODE=1 SHARED=0 RUNTIME_LIBS=static
 
+cd ../../../../hugin/utils
