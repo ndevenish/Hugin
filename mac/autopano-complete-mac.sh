d10b75aa8cf6775af64e3955e6da063652b09907
ippei <ippei@users.sourceforge.net>
2005-05-17 19:36:48 +0000

    Mac: current HuginOSX files except localised files which I am changing the structure at the moment.

---
+++ mac/autopano-complete-mac.sh
@@ -0,0 +1,92 @@
+#!/bin/sh
+
+# by Pablo d'Angelo <pablo.dangelo@web.de>
+# modified by Ippei UKAI <ippei_ukai@mac.com> for HuginOSX
+
+set -e
+
+MONO=$(which mono)
+
+# Set this to the directory you installed autopano-sift into, for example
+#AUTOPANO_PATH="/Users/ippei/Download/HuginOSX/autopano-sift"
+# Do not use a trailing backslash. If the executeables are within your path
+# (recommended, you can leave the line below).
+#AUTOPANO_PATH=$(dirname $(which generatekeys.exe))
+
+usage()
+{
+	echo "usage: autopano-complete.sh [options] -o panoproject.pto image1 image2 [...]"
+	echo
+	echo "  -o name      filename of created panorama project"
+	echo
+	echo "[options]:"
+	echo "  -a path      specifies path to the directory where"
+	echo "              autopano.exe and generatekeys-sd.exe are."
+	echo "  -s number    downsize images until width and height is"
+	echo "              smaller than number, default 700"
+	echo "  -p number    number of generated control points between,"
+	echo "              each pair, default: 10"
+	echo "  -n           no ransac detection, useful for fisheye images"
+	echo "  -c           do not reuse keypoints detected in earlier runs,"
+	echo "              deletes old keypoint files."
+	exit 1
+}
+
+echo `basename "$0"` "$@"
+
+
+#NARG=$#
+#if [ $NARG -lt 2 ]; then
+#	usage
+#	exit 1
+#fi
+
+args=`getopt o:a:s:p:nch $*`
+set -- $args
+
+if [ $? != 0 ] ; then echo "Terminating..." >&2 ; exit 1 ; fi
+
+
+POINTS=10;
+RANSAC=1;
+CLEAN=0;
+SIZE=800;
+
+while true ; do
+        case "$1" in
+                -o) PANOFILE=$2; shift 2;;
+		-a) AUTOPANO_PATH=$2; shift 2;;
+                -s) SIZE=$2; shift 2;;
+                -p) POINTS=$2; shift 2 ;;
+                -n) RANSAC=0; shift 1;;
+                -c) CLEAN=1; shift 1;;
+                -h) usage; shift 1;;
+                --) shift ; break ;;
+                *) echo "Command line parsing error at: $1" ; exit 1 ;;
+        esac
+done
+
+echo "Remaining arguments ($#):"
+for arg do echo '--> '"\`$arg'" ; done
+
+# Allow user to override temporary directory.
+TMP=${TMPDIR:-/tmp}
+
+KEYFILES=""
+for arg do
+        FILENAME=$(basename $arg).key.gz
+	KEYFILES="$KEYFILES $FILENAME"
+	if [ -f $FILENAME ]; then
+		if [ $CLEAN -ne 0 ]; then
+			$MONO $AUTOPANO_PATH/generatekeys-sd.exe $arg $FILENAME $SIZE
+		else
+			echo "Using previously generated keypoint file: $FILENAME"
+		fi
+	else
+		$MONO $AUTOPANO_PATH/generatekeys-sd.exe $arg $FILENAME $SIZE
+	fi
+done
+
+echo "keyfiles: $KEYFILES"
+ARG="--ransac $RANSAC --maxmatches $POINTS";
+$MONO $AUTOPANO_PATH/autopano.exe $ARG $PANOFILE $KEYFILES
---
+++ mac/config.h
@@ -0,0 +1,87 @@
+/* src/include/config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define to 1 if translation of program messages to the user's native
+   language is requested. */
+#undef ENABLE_NLS
+
+/* Define if the GNU dcgettext() function is already present or preinstalled.
+   */
+#define HAVE_DCGETTEXT
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#define HAVE_DLFCN_H
+
+/* Define if the FFTW library is available */
+#define HAVE_FFTW
+
+/* Define if the GNU gettext() function is already present or preinstalled. */
+#define HAVE_GETTEXT
+
+/* Define if you have the iconv() function. */
+#define HAVE_ICONV
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H
+
+/* Current Version of Hugin */
+#define HUGIN_VERSION  0.5
+
+/* Define if you have JPEG library */
+#define HasJPEG
+
+/* Define if you have Panotools library (pano12) */
+#define HasPANO
+
+/* Define if you have PNG library */
+#define HasPNG
+
+/* Define if you have TIFF library */
+#define HasTIFF
+
+/* Install Location of Hugin */
+#undef INSTALL_LOCALE_DIR
+
+/* Location for XRC information */
+#undef INSTALL_XRC_DIR
+
+/* Define to the address where bug reports for this package should be sent. */
+#undef PACKAGE_BUGREPORT
+
+/* Define to the full name of this package. */
+#undef PACKAGE_NAME
+
+/* Define to the full name and version of this package. */
+#undef PACKAGE_STRING
+
+/* Define to the one symbol short name of this package. */
+#undef PACKAGE_TARNAME
+
+/* Define to the version of this package. */
+#undef PACKAGE_VERSION
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS
