db7ae2112ec512a4a495a7f7a18702b5a0a8e51b
ippei <ippei@users.sourceforge.net>
2005-06-02 17:11:57 +0000

    Mac: replaces tabs in cp_editor_panel-2.5.xrc with choice. Now we don't need separate xrc files.

--- mac/localised.sh
+++ mac/localised.sh
@@ -1,20 +1,42 @@
 #!/bin/sh
 
+resdir="build/HuginOSX.app/Contents/Resources"
+xrcsrcdir="../src/hugin/xrc"
+
+rm -fR $resdir/xrc
+echo copying xrc folder to $resdir/xrc
+cp -r $xrcsrcdir $resdir/
+echo removing extra files from xrc folder
+rm -f $resdir/xrc/.??*
+rm -fR $resdir/xrc/CVS
+rm -f $resdir/xrc/data/.??*
+rm -fR $resdir/xrc/data/CVS
+
+#for xrcfile in `ls $resdir/xrc | grep mac.xrc`
+#do
+#  echo copying $xrcfile to $resdir/xrc/`echo $xrcfile|sed s/-mac.xrc/.xrc/`
+#  cp -f $xrcfile $resdir/xrc/`echo $xrcfile|sed s/-mac.xrc/.xrc/`
+#done
+
+echo patching $resdir/xrc/cp_editor_panel-2.5.xrc to use wxChoice instead of wxNotebook
+mv $resdir/xrc/cp_editor_panel-2.5.xrc $resdir/xrc/cp_editor_panel-2.5.xrc-bk
+sed -e s/wxNotebook/wxChoice/ -e s/cp_editor_left_tab/cp_editor_left_choice/ -e s/cp_editor_right_tab/cp_editor_right_choice/ $resdir/xrc/cp_editor_panel-2.5.xrc-bk > $resdir/xrc/cp_editor_panel-2.5.xrc
+
+
 for lang in `cat languages`
 do
  
  echo 
  echo "Language: $lang"
 
- resdir="build/HuginOSX.app/Contents/Resources/$lang.lproj"
- localedir="$resdir/locale"
- datasrcdir="../src/hugin/xrc/data"
+ localisedresdir="$resdir/$lang.lproj"
+ localedir="$localisedresdir/locale"
 
- rm -fR $resdir
- echo "deleting $resdir"
+ rm -fR $localisedresdir
+ echo "deleting $localisedresdir"
 
- mkdir "$resdir"
- echo "making $resdir"
+ mkdir "$localisedresdir"
+ echo "making $localisedresdir"
  mkdir "$localedir"
  echo "making $localedir"
 
@@ -28,31 +50,24 @@ do
  echo "hugin.mo to $lang.po"
  msgfmt -v -o "$localedir/hugin.mo" "../src/hugin/po/$lang.po"
 
- for file in `ls $datasrcdir | grep _$lang.htm`
+ for file in `ls $xrcsrcdir/data | grep _$lang.htm`
  do
-  echo copying $file to $resdir/`echo $file|sed s/_$lang//`
+  echo copying $file to $localisedresdir/`echo $file|sed s/_$lang//`
   echo  rewriting \'src=\"\' to \'src=\"../xrc/data/\'
-  sed s/src\=\"/src\=\"..\\/xrc\\/data\\// "$datasrcdir/$file" > $resdir/`echo $file|sed s/_$lang//`
+  sed s/src\=\"/src\=\"..\\/xrc\\/data\\// "$xrcsrcdir/data/$file" > $localisedresdir/`echo $file|sed s/_$lang//`
  done
 
- for file in `ls $datasrcdir | grep _$lang.html`
+ for file in `ls $xrcsrcdir/data | grep _$lang.html`
  do
-  echo copying $file to $resdir/`echo $file|sed s/_$lang//`
+  echo copying $file to $localisedresdir/`echo $file|sed s/_$lang//`
   echo  rewriting \'src=\"\' to \'src=\"../xrc/data/\'
-  sed s/src\=\"/src\=\"..\\/xrc\\/data\\// "$datasrcdir/$file" > $resdir/`echo $file|sed s/_$lang//`
+  sed s/src\=\"/src\=\"..\\/xrc\\/data\\// "$xrcsrcdir/data/$file" > $localisedresdir/`echo $file|sed s/_$lang//`
  done
 
- for file in `ls $datasrcdir | grep _$lang-UTF8.txt`
+ for file in `ls $xrcsrcdir/data | grep _$lang-UTF8.txt`
  do
-  echo copying $file to $resdir/`echo $file|sed s/_$lang//`
-  cp "$datasrcdir/$file" $resdir/`echo $file|sed s/_$lang//`
+  echo copying $file to $localisedresdir/`echo $file|sed s/_$lang//`
+  cp "$xrcsrcdir/data/$file" $localisedresdir/`echo $file|sed s/_$lang//`
  done
 
-done
-
-
-for xrcfile in `ls | grep mac.xrc`
-do
-  echo copying $xrcfile to build/HuginOSX.app/Contents/Resources/xrc/`echo $xrcfile|sed s/-mac.xrc/.xrc/`
- cp -f $xrcfile build/HuginOSX.app/Contents/Resources/xrc/`echo $xrcfile|sed s/-mac.xrc/.xrc/`
 done
