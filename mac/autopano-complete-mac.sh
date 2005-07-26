5833c1caa63ea6f36f9d1139e43fa4c9fa1b5708
ippei <ippei@users.sourceforge.net>
2005-07-26 16:50:33 +0000

    Oliver Beckstein has hacked around the problem with spaces in the path.

--- mac/autopano-complete-mac.sh
+++ mac/autopano-complete-mac.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-
+# Time-stamp: <2005-07-24 22:05:26 oliver>
 # by Pablo d'Angelo <pablo.dangelo@web.de>
 # modified by Ippei UKAI <ippei_ukai@mac.com> for HuginOSX
+# Oliver Beckstein 2005-07-24 <orbeckst@gmail.com>
+# - dirty hack to handle filenames with spaces 
+#   (better: reimplement in perl/python)
+
 
 set -e
 
@@ -10,6 +14,23 @@ myexit()
     sleep 10
     exit 1
 }
+
+# dirty hack to protect whitespace in filenames
+# replace temporarily with ' ' -> '__SPACE__'
+# (the same trick is later used to get quotes where they are supposed to be)
+_SPACE='__SPACE__';
+_QUOTE='__QUOTE__';
+
+function protect_space () {
+    local a="${1}"
+    echo "${a}" | sed -e "s| |${_SPACE}|g";
+}
+
+function unprotect_space () {
+    local a="${1}"
+    echo "${a}" | sed -e "s|${_SPACE}| |g";
+}
+
 
 MONO=$(which mono)
 if [ "$MONO" = "" -o ! -x "$MONO" ]
@@ -20,8 +41,9 @@ then
 fi
 
 # Set this to the directory you installed autopano-sift into, for example
-#AUTOPANO_PATH="/Users/ippei/Download/HuginOSX/autopano-sift"
-# Do not use a trailing backslash. If the executeables are within your path
+#AUTOPANO_PATH="/usr/local/lib/autopano-sift/bin"
+
+# Do not use a trailing backslash. If the executebles are within your path
 # (recommended, you can leave the line below).
 #AUTOPANO_PATH=$(dirname $(which generatekeys-sd.exe))
 AUTOPANO_PATH=`dirname "$0"`/autopano-sift
@@ -54,6 +76,16 @@ echo "AUTOPANO_PATH = \"$AUTOPANO_PATH\""
 #	exit 1
 #fi
 
+
+# dirty hack to protect space in filenames
+protected_args="";
+for a in "$@"; do
+    a=`protect_space "${a}"`;
+    protected_args="${protected_args} ${a}";
+done 
+
+set -- ${protected_args}
+
 #args=`getopt o:a:s:p:nch $*`
 args=`getopt o:s:p:nch $*`
 set -- $args
@@ -68,7 +100,7 @@ SIZE=800;
 
 while true ; do
         case "$1" in
-                -o) PANOFILE=$2; shift 2;;
+                -o) PANOFILE="`unprotect_space $2`"; shift 2;;
 #                -a) AUTOPANO_PATH=$2; shift 2;;
                 -s) SIZE=$2; shift 2;;
                 -p) POINTS=$2; shift 2 ;;
@@ -91,26 +123,44 @@ then echo error "Cannot locate generatekeys-sd.exe in $AUTOPANO_PATH"; myexit; f
 if [ ! -f "$AUTOPANO_PATH/autopano.exe" ]
 then echo error "Cannot locate autopano.exe in $AUTOPANO_PATH"; myexit; fi
 
+# DEBUGGING: set -x
+# bring back space only when filename complete 
+# (bash quoting sucks---rewrite in perl or whatever)
 KEYFILES=""
 for arg do
-        FILENAME=$(basename $arg).key.gz
+        FILENAME="$(basename $arg).key.gz"
+	# keep space-protected filenames in KEYFILES 
+        # (unprotect only when needed at end):
 	KEYFILES="$KEYFILES $FILENAME"
-	if [ -f $FILENAME ]; then
+	# unprotect filenames which are needed NOW:
+	FILENAME="`unprotect_space ${FILENAME}`"
+	arg="`unprotect_space ${arg}`"
+	if [ -f "$FILENAME" ]; then
 		if [ $CLEAN -ne 0 ]; then
-            echo "$MONO \"$AUTOPANO_PATH/generatekeys-sd.exe\" $arg $FILENAME $SIZE"
-			$MONO "$AUTOPANO_PATH/generatekeys-sd.exe" $arg $FILENAME $SIZE
+		    echo "$MONO \"$AUTOPANO_PATH/generatekeys-sd.exe\" \"$arg\" \"$FILENAME\" $SIZE"
+		    $MONO "$AUTOPANO_PATH/generatekeys-sd.exe" "$arg" "$FILENAME" $SIZE
 		else
 			echo "Using previously generated keypoint file: $FILENAME"
 		fi
 	else
-		echo "$MONO \"$AUTOPANO_PATH/generatekeys-sd.exe\" $arg $FILENAME $SIZE"
-		$MONO "$AUTOPANO_PATH/generatekeys-sd.exe" $arg $FILENAME $SIZE
+		echo "$MONO \"$AUTOPANO_PATH/generatekeys-sd.exe\" \"$arg\" \"$FILENAME\" $SIZE"
+		$MONO "$AUTOPANO_PATH/generatekeys-sd.exe" "$arg" "$FILENAME" $SIZE
 	fi
 done
 
-echo "keyfiles: $KEYFILES"
+echo "keyfiles (space protected): $KEYFILES"
 ARG="--ransac $RANSAC --maxmatches $POINTS";
-echo "$MONO \"$AUTOPANO_PATH/autopano.exe\" $ARG $PANOFILE $KEYFILES"
-$MONO "$AUTOPANO_PATH/autopano.exe" $ARG $PANOFILE $KEYFILES
+echo "$MONO \"$AUTOPANO_PATH/autopano.exe\" $ARG \"$PANOFILE\" $KEYFILES"
+
+# splice in KEYFILES with spaces
+cmd="$MONO \"$AUTOPANO_PATH/autopano.exe\" $ARG \"$PANOFILE\""
+keys=""
+# use __QUOTE__ and __SPACE__ to build final commandline
+for k in ${KEYFILES}; do
+    keys="${keys} ${_QUOTE}${k}${_QUOTE}"
+done;
+cmd=`echo "${cmd} ${keys}" | sed -e "s|${_QUOTE}|\"|g" -e "s|${_SPACE}| |g"`
+echo "Final command: '${cmd}"
+eval "${cmd}" 
 
 sleep 3;
