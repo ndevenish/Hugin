19d57c068f393bdeeabb5796644923fafa5be844
dangelo <dangelo@users.sourceforge.net>
2006-02-05 14:06:22 +0000

    added ransac code and estimator for vignetting polynomial

---
+++ src/include/PT/RandomPointSampler.h
@@ -0,0 +1,305 @@
+// -*- c-basic-offset: 4 -*-
+/** @file RandomPointSampler.h
+ *
+ *  @author Pablo d'Angelo <pablo.dangelo@web.de>
+ *
+ *  $Id$
+ *
+ *  This is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This software is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public
+ *  License along with this software; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#ifndef _RANDOM_POINT_SAMPLER_H
+#define _RANDOM_POINT_SAMPLER_H
+
+#include <fstream>
+#include <algorithm>
+#include <ctime>
+
+#include <common/math.h>
+
+#include <vigra_ext/VignettingCorrection.h>
+
+#include <PT/PanoImage.h>
+#include <PT/SpaceTransform.h>
+
+namespace PT
+{
+
+/** sample all points inside a panorama and check for create a
+ *  bins of point pairs that include a specific radius.
+ *
+ */
+template <class Img, class VoteImg>
+void sampleAllPanoPoints(const std::vector<Img> &imgs,
+                         const std::vector<VoteImg *> &voteImgs,
+                         const std::vector<SrcPanoImage> & src,
+                         DestPanoImage dest,
+                         int nPoints,
+                         typename Img::PixelType minI,
+                         typename Img::PixelType maxI,
+                         //std::vector<vigra_ext::PointPair> &points,
+                         std::vector<std::multimap<double, vigra_ext::PointPair> > & radiusHist,
+                         unsigned & nGoodPoints,
+                         unsigned & nBadPoints,
+                         utils::MultiProgressDisplay & progress)
+{
+    typedef typename Img::PixelType PixelType;
+
+    // use 10 bins
+    radiusHist.resize(10);
+    unsigned pairsPerBin = nPoints / radiusHist.size();
+
+    nGoodPoints = 0;
+    nBadPoints = 0;
+    vigra_precondition(imgs.size() > 1, "sampleAllPanoPoints: At least two images required");
+    vigra_precondition(imgs.size() == src.size(), "number of src images doesn't match");
+    
+    unsigned nImg = imgs.size();
+
+    vigra::Size2D srcSize = src[0].getSize();
+    double maxr = sqrt(((double)srcSize.x)*srcSize.x + ((double)srcSize.y)*srcSize.y) / 2.0;
+
+    // create an array of transforms.
+    std::vector<SpaceTransform> transf(imgs.size());
+
+    // initialize transforms, and interpolating accessors
+    for(unsigned i=0; i < imgs.size(); i++) {
+        vigra_precondition(src[i].getSize() == srcSize, "images need to have the same size");
+        transf[i].createTransform(src[i], dest);
+    }
+
+    for (int y=dest.getROI().top(); y < dest.getROI().bottom(); ++y) {
+        for (int x=dest.getROI().left(); x < dest.getROI().right(); ++x) {
+            FDiff2D panoPnt(x,y);
+            for (unsigned i=0; i< nImg; i++) {
+            // transform pixel
+                FDiff2D p1;
+                transf[i].transformImgCoord(p1, panoPnt);
+                vigra::Point2D p1Int(p1.toDiff2D());
+                // is inside:
+                if (!src[i].isInside(p1Int)) {
+                    // point is outside image
+                    continue;
+                }
+                PixelType i1;
+                if (imgs[i](p1.x,p1.y, i1)){
+                    if (minI > i1 || maxI < i1) {
+                        // ignore pixels that are too dark or bright
+                        continue;
+                    }
+                    double r1 = norm((p1 - src[i].getRadialVigCorrCenter())/maxr);
+
+                    // check inner image
+                    for (unsigned j=i+1; j < nImg; j++) {
+                        FDiff2D p2;
+                        transf[j].transformImgCoord(p2, panoPnt);
+                        vigra::Point2D p2Int(p2.toDiff2D());
+                        if (!src[j].isInside(p2Int)) {
+                            // point is outside image
+                            continue;
+                        }
+                        PixelType i2;
+                        if (imgs[j](p2.x, p2.y, i2)){
+                            if (minI > i2 || maxI < i2) {
+                                // ignore pixels that are too dark or bright
+                                continue;
+                            }
+                            double r2 = norm((p2 - src[j].getRadialVigCorrCenter())/maxr);
+                            // add pixel
+                            const VoteImg & vimg1 =  *voteImgs[i];
+                            const VoteImg & vimg2 =  *voteImgs[j];
+                            double laplace = utils::sqr(vimg1[p1Int]) + utils::sqr(vimg2[p2Int]);
+                            int bin1 = (int)(r1*10);
+                            int bin2 = (int)(r2*10);
+                            // a center shift might lead to radi > 1.
+                            if (bin1 > 9) bin1 = 9;
+                            if (bin2 > 9) bin2 = 9;
+
+                            vigra_ext::PointPair pp;
+                            if (i1 >= i2 && r1 <= r2) {
+                                // ok, point is good. i1 is closer to centre, swap point
+                                // so that i1 < i2
+                                pp = vigra_ext::PointPair(i2, p2, r2, i1, p1, r1);
+                            } else if( i1 <= i2 && r1 >= r2) {
+                                pp = vigra_ext::PointPair(i1, p1, r1, i2, p2, r2);
+                            } else {
+                                // point is inconsistent
+                                nBadPoints++;
+                                continue;
+                            }
+
+                            // decide which bin should be used.
+                            std::multimap<double, vigra_ext::PointPair> * map1 = &radiusHist[bin1];
+                            std::multimap<double, vigra_ext::PointPair> * map2 = &radiusHist[bin2];
+                            std::multimap<double, vigra_ext::PointPair> * destMap;
+                            if (map1->size() == 0) {
+                                destMap = map1;
+                            } else if (map2->size() == 0) {
+                                destMap = map2;
+                            } else if (map1->size() < map2->size()) {
+                                destMap = map1;
+                            } else if (map1->size() > map2->size()) {
+                                destMap = map2;
+                            } else if (map1->rend()->first > map2->rend()->first) {
+                                // heuristic: insert into bin with higher maximum laplacian filter response
+                                // (higher probablity of misregistration).
+                                destMap = map1;
+                            } else {
+                                destMap = map2;
+                            }
+                            // insert
+                            destMap->insert(std::make_pair(laplace,pp));
+                            // remove last element if too many elements have been gathered
+                            if (destMap->size() > pairsPerBin) {
+                                destMap->erase((--(destMap->end())));
+                            }
+                            nGoodPoints++;
+                        }
+                    }
+                }
+            }
+        }
+        int h = dest.getROI().bottom() - dest.getROI().top();
+        if (h > 100) {
+            if ((y-dest.getROI().top())%(h/20) == 0) {
+                progress.setProgress(((double)y-dest.getROI().top())/h);
+            }
+        }
+    }
+}
+
+
+/** extract some random points out of the bins.
+ *  This should ensure that the radius distribution is
+ *  roughly uniform
+ */
+void sampleRadiusUniform(const std::vector<std::multimap<double, vigra_ext::PointPair> > & radiusHist,
+                         unsigned nPoints,
+                         std::vector<vigra_ext::PointPair> &selectedPoints)
+{
+    // reserve selected points..
+    int drawsPerBin = nPoints / radiusHist.size();
+    // reallocate output vector.
+    selectedPoints.reserve(drawsPerBin*radiusHist.size());
+    for (std::vector<std::multimap<double, vigra_ext::PointPair> >::const_iterator bin= radiusHist.begin();
+         bin != radiusHist.end(); ++bin) 
+    {
+        unsigned i=drawsPerBin;
+        // copy into vector
+        for (std::multimap<double, vigra_ext::PointPair>::const_iterator it= (*bin).begin();
+             it != (*bin).end(); ++it) 
+        {
+            selectedPoints.push_back(it->second);
+            // do not extract more than drawsPerBin Point pairs.
+            --i;
+            if (i == 0)
+                break;
+        }
+    }
+}
+
+template <class Img>
+void sampleRandomPanoPoints(const std::vector<Img> &imgs,
+                            const std::vector<SrcPanoImage> & src,
+                            DestPanoImage dest,
+                            int nPoints,
+                            typename Img::PixelType minI,
+                            typename Img::PixelType maxI,
+                            std::vector<vigra_ext::PointPair> &points,
+                            unsigned & nBadPoints)
+{
+    typedef typename Img::PixelType PixelType;
+
+    vigra_precondition(imgs.size() > 1, "sampleRandomPanoPoints: At least two images required");
+    vigra_precondition(imgs.size() == src.size(), "number of src images doesn't match");
+    
+    unsigned nImg = imgs.size();
+
+    vigra::Size2D srcSize = src[0].getSize();
+    double maxr = sqrt(((double)srcSize.x)*srcSize.x + ((double)srcSize.y)*srcSize.y) / 2.0;
+
+    // create an array of transforms.
+    std::vector<SpaceTransform> transf(imgs.size());
+
+    // initialize transforms, and interpolating accessors
+    for(unsigned i=0; i < imgs.size(); i++) {
+        vigra_precondition(src[i].getSize() == srcSize, "images need to have the same size");
+        transf[i].createTransform(src[i], dest);
+    }
+    // init random number generator
+    boost::mt19937 rng;
+    // start with a different seed every time.
+    rng.seed(static_cast<unsigned int>(std::time(0)));
+    // randomly sample points.
+    boost::uniform_int<> distribx(dest.getROI().left(), dest.getROI().right()-1);
+    boost::uniform_int<> distriby(dest.getROI().top(), dest.getROI().bottom()-1);
+
+    boost::variate_generator<boost::mt19937&, boost::uniform_int<> >
+            randX(rng, distribx);             // glues randomness with mapping
+    boost::variate_generator<boost::mt19937&, boost::uniform_int<> >
+            randY(rng, distriby);             // glues randomness with mapping
+
+    unsigned n_good=0;
+    unsigned n_bad=0;
+    for (unsigned maxTry = nPoints*10; nPoints > 0 && maxTry > 0; maxTry--) {
+        unsigned x = randX();
+        unsigned y = randY();
+        FDiff2D panoPnt(x,y);
+        for (unsigned i=0; i< nImg; i++) {
+            // transform pixel
+            PixelType i1;
+            FDiff2D p1;
+            transf[i].transformImgCoord(p1, panoPnt);
+            // check if pixel is valid
+            if (imgs[i](p1.x,p1.y, i1)){
+                if (minI > i1 || maxI < i1) {
+                    // ignore pixels that are too dark or bright
+                    continue;
+                }
+                double r1 = norm((p1 - src[i].getRadialVigCorrCenter())/maxr);
+                for (unsigned j=i+1; j < nImg; j++) {
+                    PixelType i2;
+                    FDiff2D p2;
+                    transf[j].transformImgCoord(p2, panoPnt);
+                    // check if a pixel is inside the source image
+                    if (imgs[j](p2.x, p2.y, i2)){
+                        if (minI > i2 || maxI < i2) {
+                            // ignore pixels that are too dark or bright
+                            continue;
+                        }
+                        double r2 = norm((p2 - src[j].getRadialVigCorrCenter())/maxr);
+                        // add pixel
+                        if (i1 >= i2 && r1 <= r2) {
+                            // ok, point is good. i1 is closer to centre, swap point
+                            // so that i1 < i2
+                            points.push_back(vigra_ext::PointPair(i2, p2, r2, i1, p1, r1) );
+                            nPoints--;
+                        } else if( i1 <= i2 && r1 >= r2) {
+                            points.push_back(vigra_ext::PointPair(i1, p1, r1, i2, p2, r2) );
+                            nPoints--;
+                        } else {
+                            nBadPoints++;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+}; // namespace
+
+#endif
---
+++ src/include/vigra_ext/VigQuotientEstimator.h
@@ -0,0 +1,363 @@
+#ifndef _VIG_QUOTIENT_ESTIMATOR_H_
+#define _VIG_QUOTIENT_ESTIMATOR_H_
+
+#include <common/math.h>
+
+#include "RansacParameterEstimator.h"
+
+#include <iostream>
+#include <boost/numeric/ublas/vector.hpp>
+#include <boost/numeric/ublas/vector_proxy.hpp>
+#include <boost/numeric/ublas/triangular.hpp>
+#include <boost/numeric/ublas/matrix.hpp>
+#include <boost/numeric/ublas/lu.hpp>
+#include <boost/numeric/ublas/io.hpp>
+#include <boost/random.hpp>
+
+#include <vigra_ext/ROIImage.h>
+
+#include "ransac.h"
+
+namespace vigra_ext
+{
+
+struct PointPair
+{
+    PointPair()
+    {
+    }
+
+    PointPair(double i1, const FDiff2D & p1, double r1,
+              double i2, const FDiff2D & p2, double r2)
+    : i1(i1), p1(p1), r1(r1), i2(i2), p2(p2), r2(r2)
+    {
+    }
+
+    double i1;
+    FDiff2D p1;
+    double r1;
+    double i2;
+    FDiff2D p2;
+    double r2;
+};
+
+/// function to calculate the vignetting correction: 1 + p[0]*r^2 + p[1]*r^4 + p[2]*r^6
+template <class PITER>
+double calcVigCorrPoly(PITER p, double r)
+{
+    const unsigned n=3;
+    double rsq = r*r;
+    double res = p[n-1] * rsq; 
+    for (int j=n-2; j >=0; j--) 
+    {
+       res = (res + p[j]) * rsq;
+    }
+    // add constant offset
+    return res + 1;
+}
+
+
+// structure with various information about the fitting process
+struct VigQuotientEstimateResult
+{
+    int nUsedPoints;
+    double brightnessRMSE;
+};
+
+/**
+ * This class estimates the parameters of the vignetting curve.
+ *
+ * We assume the following function:
+ *  I1/I2 = (1 + p_1*r_1^2 + p_2*r_1^4 + p_3*r_1^6) / (1 + a*r_2^2 + b*r_2^4 + c*r_2^6)
+ *
+ * Author: Pablo d'Angelo
+ */
+
+
+class VigQuotientEstimator : public RansacParameterEstimator
+{
+
+protected:
+        double m_delta;
+
+public:
+
+    typedef boost::numeric::ublas::vector<double> Param;
+
+    VigQuotientEstimator (double delta)
+    : RansacParameterEstimator(3), m_delta(delta)
+    {
+    }
+
+    /**  Compute the vignetting curve defined by 3 points.
+     *
+     *  This results in the following equations:
+     *   z = I_1 / I_2
+     * 
+     *   A*p = 1-z;
+     *    with A = [ r_1^2-r_2^2  r_1^4-r_2^4   r_1^6-r_2^6 ]
+     *                   ..           ..            ..
+     * 
+     * @param data A vector containing three 2D points.
+     * @param parameters This vector is filled with the computed parameters, if fitting
+     *                   was succesfull
+     * @return fitting was succesfull
+     */
+    virtual bool estimate(const std::vector<const PointPair *> &data,
+                          Param &p) const
+    {
+        const int n=3;
+        if(data.size() < n) {
+            return false;
+        }
+        // estimate point
+        
+        // create linear equation 
+        p.resize(n);
+        boost::numeric::ublas::permutation_matrix<size_t> permut(n);
+        boost::numeric::ublas::matrix<double> A(n,n);
+
+        for (int i = 0; i < n; i++) {
+            double r1sq = data[i]->r1 * data[i]->r1;
+            double r2sq = data[i]->r2 * data[i]->r2;
+            double r1f = r1sq;
+            double r2f = r2sq;
+            for (int j=0;j < n; j++) {
+                A(i,j) = r2f - r1f;
+                r1f*=r1sq;
+                r2f*=r2sq;
+            }
+            double c = (data[i]->i1 / data[i]->i2);
+            p[i] =  1 - c;
+         }
+
+         try {
+//             std::cout << "before factorize A:" << A << std::endl << "p: " << p << std::endl;
+             lu_factorize(A,permut);
+//             std::cout << "after factorize A:" << A << std::endl << "perm: " << permut << std::endl;
+             lu_substitute(A,permut,p); 
+//             std::cout << "after substitute A:" << A << std::endl << "p: " << p << std::endl;
+         } catch (boost::numeric::ublas::singular){
+             return false;
+         }
+         return true;
+    }
+
+
+    /**
+     * Compute a least squares estimate.
+     */
+    bool leastSquaresEstimate(const std::vector<const PointPair *> &data,
+                              Param & p) const
+    {
+        /*
+        {
+            std::ofstream of("hugin_vigquot_least_sq_points");
+            for (std::vector<const PointPair *>::const_iterator it = data.begin(); it != data.end(); ++it) {
+                of << (*it)->r1 << " " << (*it)->i1 << " " << (*it)->r2 << " " << (*it)->i2 << std::endl;
+            }
+        }
+        */
+
+        const int n=3;
+        if(data.size() < n) {
+            return false;
+        }
+        // calculate least squares, based on pseudoinverse.
+        // A'A x = A' b.
+        boost::numeric::ublas::permutation_matrix<size_t> permut(n);
+        boost::numeric::ublas::matrix<double> AtA(n,n);
+        for (unsigned i=0; i< n; i++){
+            for (unsigned j=0; j< n; j++){
+                AtA(i,j) = 0;
+            }
+        }
+        // is this the right way to clear a boost matrix?
+//        AtA = boost::numeric::ublas::zero_matrix<double>(n,n);
+        p.resize(n);
+        // is this the right way to clear a boost vector?
+        for (unsigned i=0; i< n; i++){
+            p[i] = 0;
+        }
+
+        for(unsigned k=0; k < data.size(); k++) {
+            // calculate one row of A
+            double Arow[n];
+            double r1sq = data[k]->r1 * data[k]->r1;
+            double r2sq = data[k]->r2 * data[k]->r2;
+            double r1f = r1sq;
+            double r2f = r2sq;
+            for (int j=0;j < n; j++) {
+                Arow[j] = r2f - r1f;
+                r1f*=r1sq;
+                r2f*=r2sq;
+            }
+            double c = (data[k]->i1 / data[k]->i2);
+            double bRow =  1 - c;
+
+            // add to pseudoinverse
+            for( unsigned i=0; i<n; ++i)
+            {
+                // calculate  Atb
+                p[i]+=Arow[i]*bRow;
+                for( unsigned j=0; j<n; ++j)
+                {
+                    AtA(i,j)+=Arow[i]*Arow[j];
+                }
+            }
+        }
+
+        // solve system
+        try {
+            lu_factorize(AtA,permut);
+            lu_substitute(AtA,permut,p); 
+//            std::cout << "lms solution: AtA (after solve):" << AtA << std::endl << "p: " << p << std::endl;
+        } catch (boost::numeric::ublas::singular){
+             return false;
+         }
+         return true;
+    }
+
+    /**
+     * Return true if the distance between the line defined by the parameters and the
+     * given point is smaller than 'delta' (see constructor).
+     * @param parameters The line parameters [n_x,n_y,a_x,a_y].
+     * @param data Check that the distance between this point and the line is smaller than 'delta'.
+     */
+    bool agree(Param &p, const PointPair &data) const
+    {
+        const int n = 3;
+
+        double r1sq = data.r1 * data.r1;
+        double r2sq = data.r2 * data.r2;
+        double poly1 = p[n-1] * r1sq; 
+        double poly2 = p[n-1] * r2sq; 
+        for (int j=n-2; j >=0; j--) 
+        {
+            poly1 = (poly1 + p[j]) * r1sq;
+            poly2 = (poly2 + p[j]) * r2sq;
+        }
+        poly1 += 1;
+        poly2 += 1;
+        double e;
+        if (data.r1 < data.r2) {
+            // I1 should be bigger than I2
+            e = data.i2 / data.i1 - poly2/poly1;
+        } else {
+            e = data.i1 / data.i2 - poly1/poly2;
+        }
+        return fabs(e) < m_delta;
+    }
+};
+
+
+
+template<class ImageType, class CoordType>
+void extractRandomPoints(std::vector<vigra_ext::ROIImage<ImageType, vigra::BImage> *>  &remapped,
+                         std::vector<CoordType> & imgXCoord,
+                         std::vector<CoordType> & imgYCoord,
+                         const std::vector<vigra::Size2D> & imgSize,
+                         const std::vector<FDiff2D> & imgCenter,
+                         unsigned nPointsPerOverlap,
+                         std::vector<PointPair> & points,
+                         unsigned & nBadPoints)
+{
+    assert(remapped.size() == imgXCoord.size());
+    assert(remapped.size() == imgYCoord.size());
+
+    // init random number generator
+    boost::mt19937 rng;
+    unsigned nImg = remapped.size();
+    nBadPoints = 0;
+    // extract points from all overlaps.
+    for (unsigned i=0; i < nImg; i++) {
+        for (unsigned j=i+1; j < nImg; j++) {
+            // get unifiying region.
+            vigra::Rect2D roi1 = remapped[i]->boundingBox();
+            vigra::Rect2D roi2 = remapped[j]->boundingBox();
+            vigra::Rect2D roi = roi1 & roi2;
+            if (roi.isEmpty()) {
+                continue;
+            }
+
+            double maxr1 = sqrt(((double)imgSize[i].x)*imgSize[i].x + ((double)imgSize[i].y)*imgSize[i].y) / 2.0;
+            double maxr2 = sqrt(((double)imgSize[i].x)*imgSize[i].x + ((double)imgSize[i].y)*imgSize[i].y) / 2.0;
+            // randomly sample points.
+            boost::uniform_int<> distribx(roi.left(), roi.right()-1);
+            boost::uniform_int<> distriby(roi.top(), roi.bottom()-1);
+
+            boost::variate_generator<boost::mt19937&, boost::uniform_int<> >
+                    randX(rng, distribx);             // glues randomness with mapping
+            boost::variate_generator<boost::mt19937&, boost::uniform_int<> >
+                    randY(rng, distriby);             // glues randomness with mapping
+
+            for (unsigned maxTry = nPointsPerOverlap*2; nPointsPerOverlap > 0 && maxTry > 0; maxTry--) {
+                unsigned x = randX();
+                unsigned y = randY();
+                if (remapped[i]->getMask(x,y) && remapped[j]->getMask(x,y)) {
+                    // extract gray value pair..
+                    FDiff2D p1(imgXCoord[i](x-roi1.left(), y-roi1.top()),
+                               imgYCoord[i](x-roi1.left(), y-roi1.top()));
+                    FDiff2D p1c = p1 - imgCenter[i];
+                    p1 = p1/maxr1;
+                    p1c = p1c/maxr1;
+                    FDiff2D p2(imgXCoord[j](x-roi2.left(), y-roi2.top()),
+                               imgYCoord[j](x-roi2.left(), y-roi2.top()));
+                    FDiff2D p2c = p2 - imgCenter[j];
+                    p2 = p2/maxr2;
+                    p2c = p2c/maxr2;
+                    double r1 = sqrt(p1c.x*p1c.x + p1c.y*p1c.y);
+                    double r2 = sqrt(p2c.x*p2c.x + p2c.y*p2c.y);
+                    double i1 = remapped[i]->operator()(x,y);
+                    double i2 = remapped[j]->operator()(x,y);
+                    if (i1 >= i2 && r1 <= r2) {
+                        // ok, point is good. i1 is closer to centre, swap point
+                        // so that i1 < i2
+                        points.push_back(PointPair(i2, p2, r2, i1, p1, r1) );
+                    } else if(i1 < i2 && r1 > r2) {
+                        points.push_back(PointPair(i1, p1, r1, i2, p2, r2) );
+                    } else {
+                        nBadPoints++;
+                    }
+
+                    nPointsPerOverlap--;
+                }
+            }
+        }
+    }
+    DEBUG_DEBUG("random point extraction: consistent: " << points.size()
+                << " inconsistent: " << nBadPoints << " points");
+}
+
+VigQuotientEstimateResult
+optimizeVignettingQuotient(const std::vector<PointPair> & points,
+                           double ransacDelta,
+                           boost::numeric::ublas::vector<double> & vigCoeff)
+{
+    VigQuotientEstimateResult res;
+
+    // do optimisation..
+    // the quotient error should be < 0.1
+    VigQuotientEstimator vqEst(ransacDelta);
+
+    res.nUsedPoints = Ransac::compute(vigCoeff, vqEst, points, 0.99, 0.3);
+
+    double sqerror=0;
+    // calculate brightness RMSE
+    // not a real residual, since the brightness error was not minimized. instead
+    // the quotient error was minimized. However, a brightness difference is observeable
+    // in the final panorama and can be intuitively understood
+    // (well, actually its the RMSE, but thats a detail ;)
+    for (std::vector<PointPair>::const_iterator pnt=points.begin(); pnt != points.end();
+         pnt++)
+    {
+        sqerror += utils::sqr(calcVigCorrPoly(vigCoeff, pnt->r1)*pnt->i1 - calcVigCorrPoly(vigCoeff, pnt->r2)*pnt->i2);
+    }
+    res.brightnessRMSE = sqrt(sqerror/points.size());
+    return res;
+}
+
+
+}
+
+#endif //_VIG_QUOTIENT_PARAM_ESTIMATOR_H_
