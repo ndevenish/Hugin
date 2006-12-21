fe763ae17d3ac4c7eb309f3dd397157d4638e562
dangelo <dangelo@users.sourceforge.net>
2006-12-21 22:50:58 +0000

    enable Create control points button as soon as two images are loaded.

--- src/hugin/ImagesPanel.cpp
+++ src/hugin/ImagesPanel.cpp
@@ -240,7 +240,11 @@ void ImagesPanel::panoramaImagesChanged(PT::Panorama &pano, const PT::UIntSet & 
 	if (pano.getNrOfImages() == 0)
 	{
 	  DisableImageCtrls();
-	}
+    };
+    if (pano.getNrOfImages() > 1) {
+        m_matchingButton->Enable();
+    }
+
     if (selected.size() == 1 &&
         *(selected.begin()) < pano.getNrOfImages() &&
         set_contains(_imgNr, *(selected.begin())))
@@ -465,7 +469,7 @@ void ImagesPanel::EnableImageCtrls()
     if (XRCCTRL(*this, "images_text_yaw", wxTextCtrl)->Enable()) {
         XRCCTRL(*this, "images_text_roll", wxTextCtrl) ->Enable();
         XRCCTRL(*this, "images_text_pitch", wxTextCtrl) ->Enable();
-        m_matchingButton->Enable();
+        // m_matchingButton->Enable();
         m_removeCPButton->Enable();
         m_moveDownButton->Enable();
         m_moveUpButton->Enable();
