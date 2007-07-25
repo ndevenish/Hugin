b752ccfaf3d7c5874a02cb4e506db9f4ad1b718d
dangelo <dangelo@users.sourceforge.net>
2007-07-25 17:06:46 +0000

    fixed calculate fov algorithm

--- src/hugin_base/algorithms/nona/CalculateFOV.h
+++ src/hugin_base/algorithms/nona/CalculateFOV.h
@@ -55,7 +55,7 @@ class CalculateFOV : public PanoramaAlgorithm
         ///
         bool runAlgorithm()
         {
-            calcFOV(o_panorama);
+            o_resultFOV = calcFOV(o_panorama);
             return true; // let's hope so.
         }
           
