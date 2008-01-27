8dadfb25af5b6b97dbbcb46c74333f73b3974289
ippei <ippei@users.sourceforge.net>
2008-01-27 19:57:29 +0000

    Mac: Xcode to build with wx28, CG disable hack enabled for all targets in wx28 script

--- mac/ExternalPrograms/scripts/wxmac28.sh
+++ mac/ExternalPrograms/scripts/wxmac28.sh
@@ -92,13 +92,14 @@ do
 # sed -e "s/-dynamiclib/-dynamiclib -arch $ARCH/g" "$patch_file-bk" > "$patch_file";
  
  # disable core graphics implementation for 10.3
- if [[ $TARGET == *darwin7 ]]
- then
+ #  disabled for all targets for now.
+ #if [[ $TARGET == *darwin7 ]]
+ #then
   echo '#ifndef wxMAC_USE_CORE_GRAPHICS'    >> lib/wx/include/mac-unicode-release-$WXVERSION/wx/setup.h
   echo ' #define wxMAC_USE_CORE_GRAPHICS 0' >> lib/wx/include/mac-unicode-release-$WXVERSION/wx/setup.h
   echo '#endif'                             >> lib/wx/include/mac-unicode-release-$WXVERSION/wx/setup.h
   echo ''                                   >> lib/wx/include/mac-unicode-release-$WXVERSION/wx/setup.h
- fi
+ #fi
 
  make clean;
 
