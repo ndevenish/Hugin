622b3fb8f849a168a8c76323f3ff0e4bf644e490
yuv <yuv@users.sourceforge.net>
2008-02-26 14:23:48 +0000

    added builder information to SVN version

--- src/hugin_version.h.in.cmake
+++ src/hugin_version.h.in.cmake
@@ -5,7 +5,7 @@
 #define VERSION_PATCH ${V_PATCH}
 #define HUGIN_WC_REVISION ${HUGIN_WC_REVISION}
 
-#define PACKAGE_VERSION "${HUGIN_PACKAGE_VERSION}"
+#define PACKAGE_VERSION "${HUGIN_PACKAGE_VERSION} built by ${HUGIN_BUILDER}"
 
 /* this is an svn checkout, tag is as such
  * all builds from SVN will be considered development versions
