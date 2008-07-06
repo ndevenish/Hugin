0b38d430e984b34510b4c88dda49112e231ee062
harryvanderwolf <harryvanderwolf@users.sourceforge.net>
2008-07-06 13:59:01 +0000

    wxmac28.sh default set to 2.8.8. No longer needs #define wxMAC_USE_CORE_GRAPHICS 0

--- mac/ExternalPrograms/scripts/wxmac28.sh
+++ mac/ExternalPrograms/scripts/wxmac28.sh
@@ -93,10 +93,10 @@ do
 # # disable core graphics implementation for 10.3
 # if [[ $TARGET == *darwin7 ]]
 # then
-  echo '#ifndef wxMAC_USE_CORE_GRAPHICS'    >> lib/wx/include/mac-unicode-release-$WXVERSION/wx/setup.h
-  echo ' #define wxMAC_USE_CORE_GRAPHICS 0' >> lib/wx/include/mac-unicode-release-$WXVERSION/wx/setup.h
-  echo '#endif'                             >> lib/wx/include/mac-unicode-release-$WXVERSION/wx/setup.h
-  echo ''                                   >> lib/wx/include/mac-unicode-release-$WXVERSION/wx/setup.h
+#  echo '#ifndef wxMAC_USE_CORE_GRAPHICS'    >> lib/wx/include/mac-unicode-release-$WXVERSION/wx/setup.h
+#  echo ' #define wxMAC_USE_CORE_GRAPHICS 0' >> lib/wx/include/mac-unicode-release-$WXVERSION/wx/setup.h
+#  echo '#endif'                             >> lib/wx/include/mac-unicode-release-$WXVERSION/wx/setup.h
+#  echo ''                                   >> lib/wx/include/mac-unicode-release-$WXVERSION/wx/setup.h
 # fi
 
  make clean;
