e038dda389426c42d9cc85ebbefa0b02eedb6376
ippei <ippei@users.sourceforge.net>
2008-07-31 14:21:25 +0000

    Mac: replace builder variable properly (currently empty)

--- mac/patch-xrc.sh
+++ mac/patch-xrc.sh
@@ -3,6 +3,7 @@
 # $Id: localised.sh 2004 2007-05-11 00:17:50Z ippei $
 
 huginVer="$HUGIN_PACKAGE_VERSION"
+huginBuilder="$HUGIN_BUILDER"
 resdir="$TARGET_BUILD_DIR/Hugin.app/Contents/Resources"
 huginsrcdir="../src/hugin1/hugin"
 xrcsrcdir="$huginsrcdir/xrc"
@@ -31,5 +32,7 @@ sed -e s/wxALL// $resdir/xrc/main_frame.xrc-bk > $resdir/xrc/main_frame.xrc
 rm $resdir/xrc/main_frame.xrc-bk
 
 echo "generating about.htm from about.htm.in"
-sed -e "s/\${HUGIN_PACKAGE_VERSION}/$huginVer/g" $resdir/xrc/data/about.htm.in > $resdir/xrc/data/about.htm
+sed -e "s/\${HUGIN_PACKAGE_VERSION}/$huginVer/g" \
+    -e "s/\${HUGIN_BUILDER}/$huginBuilder/g" \
+    $resdir/xrc/data/about.htm.in > $resdir/xrc/data/about.htm
 rm $resdir/xrc/data/about.htm.in
