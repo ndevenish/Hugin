8016f6cec2f26feb21527d0a4d80cb31281353a2
tmodes <tmodes@users.sourceforge.net>
2009-05-19 05:24:03 +0000

    Add license header and author for CPImagesComboBox and ResetDialog

--- src/hugin1/hugin/ResetDialog.cpp
+++ src/hugin1/hugin/ResetDialog.cpp
@@ -1,4 +1,30 @@
-/** implementation of ResetDialog */
+// -*- c-basic-offset: 4 -*-
+
+/** @file ResetDialog.cpp
+ *
+ *	@brief implementation of ResetDialog class
+ *
+ *  @author Thomas Modes
+ *
+ *  $Id: ResetDialog.cpp 5860 2009-05-18 22:24:11Z tmodes $
+ *
+ */
+
+/*  This is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This software is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public
+ *  License along with this software; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
 
 #include "hugin/ResetDialog.h"
 #include "common/wxPlatform.h"
--- src/hugin1/hugin/ResetDialog.h
+++ src/hugin1/hugin/ResetDialog.h
@@ -1,8 +1,15 @@
 // -*- c-basic-offset: 4 -*-
-/*  @file ResetDialog.h
+/**  @file ResetDialog.h
  *
+ *  @brief Definition of ResetDialog class
  *
- *  This is free software; you can redistribute it and/or
+ *  @author Thomas Modes
+ *
+ *  $Id: ResetDialog.h 5860 2009-05-18 22:24:11Z tmodes $
+ *
+ */
+
+/*  This is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU General Public
  *  License as published by the Free Software Foundation; either
  *  version 2 of the License, or (at your option) any later version.
