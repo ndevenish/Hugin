b1d837c92c92997285835e86737d1f905f716120
yuv <yuv@users.sourceforge.net>
2009-09-10 16:24:24 +0000

    applying trunk 4360

--- src/hugin1/tests/test_projections.cpp
+++ src/hugin1/tests/test_projections.cpp
@@ -1,3 +1,25 @@
+/** @file test_projections.cpp
+ *
+ *  @common Simple test for the forward and backward translation
+ *
+ *  @author Pablo d'Angelo <pablo.dangelo@web.de>
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This software is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public
+ *  License along with this software; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
 #include "PT/PanoToolsInterface.h"
 
 using namespace PT;
