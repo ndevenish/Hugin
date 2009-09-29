abfff3ec00ed2a4123677ba6f7c44f5583c3afbe
harryvanderwolf <harryvanderwolf@users.sourceforge.net>
2009-09-29 12:20:26 +0000

    [OSX] make panomatic.sh script also work on Leopard for Tiger

--- mac/ExternalPrograms/scripts/panomatic.sh
+++ mac/ExternalPrograms/scripts/panomatic.sh
@@ -46,43 +46,62 @@ do
  ARCHARGs=""
  MACSDKDIR=""
 
- OTHERARGs="-fast -ffast-math -mfpmath=sse -msse3"
-# OTHERMAKEARGS="j4"
-# i386ExtraARGS="-mfpmath=sse -msse3"
+ OTHERARGs="-fast -ffast-math"
 
  if [ $ARCH = "i386" -o $ARCH = "i686" ]
  then
   TARGET=$i386TARGET
   MACSDKDIR=$i386MACSDKDIR
   ARCHARGs="$i386ONLYARG"
+  OTHERCARGS="-mmacosx-version-min=10.4"
+  OTHERLDARGS="-mmacosx-version-min=10.4"
+  export CC=$I386CC;
+  export CXX=$I386CXX;
  elif [ $ARCH = "ppc" -o $ARCH = "ppc750" -o $ARCH = "ppc7400" ]
  then
   TARGET=$ppcTARGET
   MACSDKDIR=$ppcMACSDKDIR
   ARCHARGs="$ppcONLYARG"
+  OTHERCARGS="-mmacosx-version-min=10.4"
+  OTHERLDARGS="-mmacosx-version-min=10.4"
+  export CC="$ppcCC -arch $ARCH";
+  export CXX="$ppcCXX -arch $ARCH";
  elif [ $ARCH = "ppc64" -o $ARCH = "ppc970" ]
  then
   TARGET=$ppc64TARGET
   MACSDKDIR=$ppc64MACSDKDIR
   ARCHARGs="$ppc64ONLYARG"
+  OTHERCARGS=""
+  OTHERLDARGS=""
+  export CC=$ppc64CC;
+  export CXX=$ppc64CXX;
  elif [ $ARCH = "x86_64" ]
  then
   TARGET=$x64TARGET
   MACSDKDIR=$x64MACSDKDIR
   ARCHARGs="$x64ONLYARG"
+  OTHERCARGS="-mtune=nocona -mfpmath=sse -msse3 -m64"
+  OTHERLDARGS=""
+  export CC=$x64CC;
+  export CXX=$x64CXX;
  fi
 
- env CFLAGS="-isysroot $MACSDKDIR -arch $ARCH $OTHERARGs $ARCHARGs $OTHERARGs -O2 -dead_strip" \
+ env CFLAGS="-isysroot $MACSDKDIR -arch $ARCH $OTHERARGs $ARCHARGs $OTHERARGs -O3 -dead_strip" \
   CXXFLAGS="-isysroot $MACSDKDIR -arch $ARCH $OTHERARGs $ARCHARGs $OTHERARGs -dead_strip" \
-  CPPFLAGS="-I$REPOSITORYDIR/include -I$REPOSITORYDIR/include/OpenEXR" \
-  LDFLAGS="-L$REPOSITORYDIR/lib -dead_strip" \
+  CPPFLAGS="-isysroot $MACSDKDIR -arch $ARCH $OTHERARGs $OTHERCARGS -I$REPOSITORYDIR/include -I$REPOSITORYDIR/include/OpenEXR" \
+  LDFLAGS="-isysroot $MACSDKDIR $OTHERLDARGS -arch $ARCH -L$REPOSITORYDIR/lib -dead_strip" \
   NEXT_ROOT="$MACSDKDIR" \
-  ./configure --prefix="$REPOSITORYDIR" --disable-dependency-tracking  \
-  --host="$TARGET" --exec-prefix=$REPOSITORYDIR/arch/$ARCH \
   ;
 
+
+  rm config.status;
+
+  ./configure --prefix="$REPOSITORYDIR" --disable-dependency-tracking  \
+  --host="$TARGET" --exec-prefix=$REPOSITORYDIR/arch/$ARCH  \
+  --with-boost=$REPOSITORYDIR/include;
+
  make clean;
- make $OTHERMAKEARGS all;
+ make -j 4;
  make  install;
 
 done
@@ -95,7 +114,7 @@ do
 
  if [ $NUMARCH -eq 1 ]
  then
-  mv "$REPOSITORYDIR/arch/$ARCHS/$program" "$REPOSITORYDIR/$program";
+  mv "$REPOSITORYDIR/arch/$ARCH/$program" "$REPOSITORYDIR/$program";
   strip "$REPOSITORYDIR/$program";
   continue
  fi
