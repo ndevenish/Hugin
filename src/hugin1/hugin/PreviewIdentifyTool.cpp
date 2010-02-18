b90c294dce6667e17e717cdc7510caca80a4efbb
yuv <yuv@users.sourceforge.net>
2010-02-18 04:23:35 +0000

    fix https://sourceforge.net/tracker/?func=detail&atid=550441&aid=2952797&group_id=77506

--- src/hugin1/hugin/PreviewIdentifyTool.cpp
+++ src/hugin1/hugin/PreviewIdentifyTool.cpp
@@ -30,10 +30,10 @@
 #include "MainFrame.h"
 
 #include <wx/platform.h>
+#include <GL/glew.h>
 #ifdef __WXMSW__
 //multitexture feature requires glew on windows
 #define GLEW_STATIC
-#include <GL/glew.h>
 #endif
 #ifdef __WXMAC__
 #include <OpenGL/gl.h>
