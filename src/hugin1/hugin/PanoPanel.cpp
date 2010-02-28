53e9923f984ff0de0336a3c5bd1c0f6b984faede
brunopostle <brunopostle@users.sourceforge.net>
2010-02-28 21:44:14 +0000

    merge trunk -r 5027 Set blender and fusion options to default value when user entered an empty string (Thomas Modes)

--- src/hugin1/hugin/PanoPanel.cpp
+++ src/hugin1/hugin/PanoPanel.cpp
@@ -814,6 +814,10 @@ void PanoPanel::OnBlenderOptions(wxCommandEvent & e)
             if (enblend_opts_text->GetValue().length() > 0) {
                 opt.enblendOptions = enblend_opts_text->GetValue().mb_str(wxConvLocal);
             }
+            else
+            {
+                opt.enblendOptions = wxConfigBase::Get()->Read(wxT("Enblend/Args"),wxT(HUGIN_ENBLEND_ARGS)).mb_str(wxConvLocal);
+            };
             GlobalCmdHist::getInstance().addCommand(
                 new PT::SetPanoOptionsCmd( *pano, opt )
                 );
@@ -842,6 +846,10 @@ void PanoPanel::OnFusionOptions(wxCommandEvent & e)
         if (enfuse_opts_text->GetValue().length() > 0) {
             opt.enfuseOptions = enfuse_opts_text->GetValue().mb_str(wxConvLocal);
         }
+        else
+        {
+            opt.enfuseOptions = wxConfigBase::Get()->Read(wxT("Enfuse/Args"),wxT(HUGIN_ENFUSE_ARGS)).mb_str(wxConvLocal);
+        };
         GlobalCmdHist::getInstance().addCommand(
             new PT::SetPanoOptionsCmd( *pano, opt )
             );
