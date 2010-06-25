164dc6a4ac86ea406e44e89f46a124b866768b8e
harryvanderwolf <hvdwolf@gmail.com>
2010-06-25 12:55:29 +0200

    [OSX] Considerable change of design of the XCode project to facilitate building of 32bit vs. 32/64 bit vs. 64bit and other architecures from one project out of multiple folders. This wasn't possible with the original project as it completely ruined the internal library paths. Copies of binaries and libraries are now done via scripts.

--- mac/copyTools.sh
+++ mac/copyTools.sh
@@ -2,33 +2,19 @@
 
 # $Id: copyTools.sh $
 
-# This script will copy all necessary tools to either the MacOS folder
-# inside Hugin.app or to the HuginStitchProject.app MacOS folder
+# This script will copy the necessary tools to the MacOS folder
+# inside the HuginStitchProject.app
+# This copy step has been removed from the project itself to facilitate
+# the 32bit and 32/64bit builds from different folders
 
 bin_dir="$REPOSITORY_DIR/bin"
 App="$TARGET_BUILD_DIR/$PRODUCT_NAME.app"
-HSPAppPath="$App/Contents/Resources/HuginStitchProject.app/Contents/MacOS"
 
+HSPbins="PTBlender PTmasker PTmender PToptimizer PTroller gnumake enblend enfuse"
 
-Hbins="celeste_standalone pano_trafo vig_optimize autooptimiser cpclean fulla pto2mk calibrate_lens deghosting_mask matchpoint tca_correct pano_modify pto_merge"
-HSPbins="exiv2"
-HSPBins2="hugin_hdrmerge nona cplean"
-
-# Copy to MacOS folder in Hugin.app
-for bins in $Hbins
-do
- cp -Rf $TARGET_BUILD_DIR/$bins "$App/Contents/MacOS/"
-done
-
-# Copy to MacOS folder in HuginStitchProject.app from External folder
+# Copy to MacOS folder in HuginStitch.app
 for bins in $HSPbins
 do
- cp -Rf $bin_dir/$bins "$HSPAppPath"
-done
-
-# Copy to MacOS folder in HuginStitchProject.app from Build dir
-for bins in $HSPbins2
-do
- cp -Rf $TARGET_BUILD_DIR/$bins "$HSPAppPath"
+ cp -Rf $bin_dir/$bins "$App/Contents/MacOS/"
 done
 
