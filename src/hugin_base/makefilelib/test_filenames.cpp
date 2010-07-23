a9d0749d845efa75573b220a8370e817de26aab7
Florian Achleitner <florian.achleitner.2.6.31@gmail.com>
2010-07-23 23:35:14 +0200

    filename tester basically working.

--- src/hugin_base/makefilelib/test_filenames.cpp
+++ src/hugin_base/makefilelib/test_filenames.cpp
@@ -19,6 +19,7 @@
 #include "Newline.h"
 #include "Rule.h"
 #include "Conditional.h"
+#include "StringAdapter.h"
 
 #include <iostream>
 #include <locale>
@@ -57,27 +58,35 @@ int createfiles_make(const path dir, char_type limit)
 {
 	const path makefile(cstr("makefile"));
 	int err = 0;
-#if 0
-	wchar_t c[] = L"X.1";
+	char_type c[] = cstr("X.1");
 	for(*c = 0x20; *c < limit; (*c)++)
 	{
 		path filename(c);
 		ofstream makefilefile(dir / makefile);
 
-		makefile::Variable mffilename("FILENAME", (dir / filename).string());
+		makefile::Variable mffilename(cstr("FILENAME"), (dir / filename).string(), makefile::Makefile::SHELL);
 		makefile::Rule touch;
-		touch.addTarget(mffilename.getRef());
-		touch.addCommand("touch " + mffilename.getRef());
+		touch.addTarget(cstr("all"));
+		touch.addCommand(cstr("@touch ") + mffilename.getRef().toString());
 
+		mffilename.getDef().add();
+		touch.add();
 		makefile::Makefile::getSingleton().writeMakefile(makefilefile);
 		makefile::Makefile::clean();
+		makefilefile.close();
+
+		string dirstring = dir.string();
+		std::string command("cd " + StringAdapter(dirstring) + " && make");
+		int makeerr = std::system(command.c_str());
+
+		if(makeerr)
+			std::cerr << "make returned " << makeerr << std::endl;
 
 		if(!fs::exists(dir / filename))
 		{
 			err++;
 		}
 	}
-#endif
 	return err;
 }
 
@@ -86,20 +95,35 @@ int main(int argc, char *argv[])
 	// set the environments locale.
 	std::locale::global(std::locale(""));
 
-	const char_type limit = 1024;
-	path basepath(cstr("/tmp/chartest"));
+	const char_type limit = 50;
+	path basepath(cstr("/tmp/chartest_direct"));
+	path basepathmake(cstr("/tmp/chartest_make"));
 
 	if(fs::is_directory(basepath))
 		fs::remove_all(basepath);
 	if(!fs::create_directories(basepath))
 	{
-		std::cerr << "Error creating directory " /*<< basepath.string()*/ << std::endl;
+		string pathstr = basepath.string();
+		std::cerr << "Error creating directory " << StringAdapter(pathstr) << std::endl;
+		return 1;
+	}
+	if(fs::is_directory(basepathmake))
+			fs::remove_all(basepathmake);
+	if(!fs::create_directories(basepathmake))
+	{
+		string pathstr = basepathmake.string();
+		std::cerr << "Error creating directory " << StringAdapter(pathstr) << std::endl;
 		return 1;
 	}
 
 	ofstream outfile(basepath / cstr("tf.out"));
 	printchars(outfile, limit);
+
 	int err = createfiles_direct(basepath, limit);
-	std::cout << "Missing files " << err << std::endl;
+	std::cout << "Direct: Missing files " << err << std::endl;
+
+
+	err = createfiles_make(basepathmake, limit);
+	std::cout << "Make: Missing files " << err << std::endl;
 	return 0;
 }
