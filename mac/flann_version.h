e9bf13e85635631348263f4c0b2f2b06acd1dd9a
harryvanderwolf <hvdwolf@gmail.com>
2011-01-16 11:35:16 +0100

    [OSX] add new flann code to XCode project

---
+++ mac/flann_version.h
@@ -0,0 +1,7 @@
+/***********************************************************************
+* This "semi" include flan_version.h is neccessary as prefix header in *
+* the Xcode project. A prefix header can be added to the target and    *
+* will be used by source files inside the target without having to     *
+* declare it within every source file                                 */
+
+#define _FLANN_VERSION 1.6.6
