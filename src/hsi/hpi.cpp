daf1b1b87b40a6e7cca2119faaadbb6574e648ca
Kay F. Jahnke <_kfj@yahoo.com>
2011-01-19 09:46:16 +0100

    added GPL headlines to some files and rewrote the documentation

--- src/hsi/hpi.cpp
+++ src/hsi/hpi.cpp
@@ -1,3 +1,28 @@
+/** @file hpi_classes.h
+ *
+ *  @brief C++ call interface to hpi
+ *
+ *  @author Kay F. Jahnke
+ *
+ */
+
+/*  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This software is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public
+ *  License along with this software; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *  KFJ 2011-01-18
+ * 
+ */
 // hpi.cpp is the code for the part of hpi that has to be
 // linked to any application using the hsi types to enable
 // it to call python plugins. This separates the implementation
--- src/hsi/hpi_classes.h
+++ src/hsi/hpi_classes.h
@@ -1,3 +1,28 @@
+/** @file hpi_classes.h
+ *
+ *  @brief core classes of the hpi interface, not for user code
+ *
+ *  @author Kay F. Jahnke
+ *
+ */
+
+/*  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This software is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public
+ *  License along with this software; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *  KFJ 2011-01-18
+ * 
+ */
 // hpi_classes.h defines the Python interface. It's not meant to
 // be included by hugin code except for hpi.cpp, which holds
 // the single instance of class python_interface and provides
