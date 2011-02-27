56f832d3cd9eb7a554968f49b2b9fb3ac4dd3c4b
dmakreshanski <dmakreshanski@users.sourceforge.net>
2011-02-27 17:29:50 +0100

    toggle buttons for images in fast preview now have variable width from 0-9, 10-99 and 100+

--- src/hugin1/hugin/GLPreviewFrame.cpp
+++ src/hugin1/hugin/GLPreviewFrame.cpp
@@ -1030,10 +1030,17 @@ void GLPreviewFrame::panoramaImagesChanged(Panorama &pano, const UIntSet &change
                 wxBoxSizer * siz = new wxBoxSizer(wxVERTICAL);
                 pan->SetSizer(siz);
 #ifdef USE_TOGGLE_BUTTON
+                unsigned int buttonsize = 20;
+                if (imgNr >= 10) {
+                    buttonsize = 27;
+                }
+                if (imgNr >= 100) {
+                    buttonsize = 34;
+                }
                 wxToggleButton * but = new wxToggleButton(pan,
                                                           ID_TOGGLE_BUT + *it,
                                                           wxString::Format(wxT("%d"),*it),
-                                                          wxDefaultPosition, wxSize(20,-1),
+                                                          wxDefaultPosition, wxSize(buttonsize,-1),
                                                           wxBU_EXACTFIT);
 #else
                 wxCheckBox * but = new wxCheckBox(pan,
