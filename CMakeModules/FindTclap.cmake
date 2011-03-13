669ed2693b4af57ee7e38cad1f4e09c5bc81e504
tmodes <devnull@localhost>
2011-03-13 09:14:18 +0100

    More general approach for finding Tclap [703735]

--- CMakeModules/FindTclap.cmake
+++ CMakeModules/FindTclap.cmake
@@ -1,13 +1,12 @@
 # search for tclap Templatized C++ Command Line Parser
 
-IF(WIN32)
 FIND_PATH(TCLAP_INCLUDEDIR tclap/CmdLine.h
+  /usr/local/include
+  /usr/include
   ${SOURCE_BASE_DIR}/tclap-1.2.0/include
 )
 IF(TCLAP_INCLUDEDIR)
 SET(TCLAP_FOUND TRUE)
+ELSE()
+SET(TCLAP_FOUND FALSE)
 ENDIF()
-
-ELSE(WIN32)
-  pkg_check_modules(TCLAP tclap>=1.1)
-ENDIF(WIN32)
