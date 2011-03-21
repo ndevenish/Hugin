4aa047e9539c204dd916f194511e0512ef0628e1
Bruno Postle <bruno@postle.net>
2011-03-21 23:10:13 +0000

    Manual: restore external links, add basic CSS support

--- src/hugin1/hugin/xrc/data/help_en_EN/dewiki.pl
+++ src/hugin1/hugin/xrc/data/help_en_EN/dewiki.pl
@@ -14,14 +14,20 @@ for my $file (@ARGV)
     $data =~ s/<link .*?>//gs;
     $data =~ s/<div id="jump-to-nav".*?<\/div>//gs;
     $data =~ s/<script.*?<\/script>//gs;
-    $data =~ s/<style.*?<\/style>//gs;
     $data =~ s/<h3 id="siteSub".*?<\/h3>//gs;
     $data =~ s/<table id="toc".*?<\/table>//gs;
     $data =~ s/<div class="editsection".*?<\/div>//gs;
+    $data =~ s/<div class=["']pBody["'].*?<\/div>//gs;
+    $data =~ s/<div [^>]*class=["']portlet["'].*?<\/div>//gs;
+    $data =~ s/<div class=["']generated-sidebar portlet["'].*?<\/div>//gs;
+    $data =~ s/<div id=["']searchBody["'].*?<\/div>//gs;
+    $data =~ s/<div id=["']column-one["'].*?<\/div>//gs;
     $data =~ s/<div id=["']catlinks["'].*/<\/div><\/div><\/div><\/div><\/body><\/html>/gs;
     $data =~ s/<!-- Saved in parser cache.*?-->//gs;
-    $data =~ s/<a href="http:.*?>(.*?)<\/a>/$1/gs;
+    $data =~ s/<a href="(https?:.*?)>(.*?)<\/a>/$2<a class="external" href="$1">[*]<\/a>/gs;
     $data =~ s/<!--[^>]*NewPP limit report[^>]*-->//gs;
+    $data =~ s/<div[^>]*>[[:space:]]*<\/div>//gs;
+    $data =~ s/<!-- Head Scripts -->/<style media="screen" type="text\/css" title="Screen style sheet"> \@import url(manual.css); <\/style>/s;
 
     open FILE, ">$file";
     print FILE $data;
