1b9efe01c1341712c293b124bd9a8c4643131d1a
Bruno Postle <bruno@postle.net>
2011-03-23 00:27:46 +0000

    More tweaks for generating the manual

--- src/hugin1/hugin/xrc/data/help_en_EN/dewiki.pl
+++ src/hugin1/hugin/xrc/data/help_en_EN/dewiki.pl
@@ -15,14 +15,17 @@ for my $file (@ARGV)
     $data =~ s/<div id="jump-to-nav".*?<\/div>//gs;
     $data =~ s/<script.*?<\/script>//gs;
     $data =~ s/<h3 id="siteSub".*?<\/h3>//gs;
+    $data =~ s/<form .*?<\/form>//gs;
     $data =~ s/<table id="toc".*?<\/table>//gs;
     $data =~ s/<div class="editsection".*?<\/div>//gs;
-    $data =~ s/<div class=["']pBody["'].*?<\/div>//gs;
+    $data =~ s/<div [^>]*class=["']pBody["'].*?<\/div>//gs;
     $data =~ s/<div [^>]*class=["']portlet["'].*?<\/div>//gs;
     $data =~ s/<div class=["']generated-sidebar portlet["'].*?<\/div>//gs;
     $data =~ s/<div id=["']searchBody["'].*?<\/div>//gs;
     $data =~ s/<div id=["']column-one["'].*?<\/div>//gs;
+    $data =~ s/<div id="(f-poweredbyico|f-copyrightico)".*?<\/div>//gs;
     $data =~ s/<div id=["']catlinks["'].*/<\/div><\/div><\/div><\/div><\/body><\/html>/gs;
+    $data =~ s/<ul id="f-list">.*?<\/ul>//gs;
     $data =~ s/<!-- Saved in parser cache.*?-->//gs;
     $data =~ s/<a href="(https?:[^"]*).*?>(.*?)<\/a>/$2<a class="external" href="$1">[*]<\/a>/gs;
     $data =~ s/<!--[^>]*NewPP limit report[^>]*-->//gs;
