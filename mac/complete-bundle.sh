95b03fbee8ffea62f3ce87878c4a7177b6da0f03
harryvanderwolf <hvdwolf@gmail.com>
2012-01-01 13:23:39 +0100

    [OSX] Add multiblend to bundle; Make switching boost versions more robust

--- mac/complete-bundle.sh
+++ mac/complete-bundle.sh
@@ -10,7 +10,7 @@ new_install_name_dirname="@executable_path/../$dylib_install_loc"
 App="$TARGET_BUILD_DIR/$PRODUCT_NAME.app"
 
 archs="ppc i386 x86_64"
-libs="libwx_macu-$WX_MAJOR_VERSION libwx_macu_gl-$WX_MAJOR_VERSION libpano13 $BOOST_THREAD_LIB-$BOOST_VER $BOOST_DATE_TIME_LIB-$BOOST_VER $BOOST_FILESYSTEM_LIB-$BOOST_VER $BOOST_IOSTREAMS_LIB-$BOOST_VER $BOOST_REGEX_LIB-$BOOST_VER $BOOST_SYSTEM_LIB-$BOOST_VER $BOOST_SIGNALS_LIB-$BOOST_VER libpng libtiff libjpeg libIex libImath libIlmImf libIlmThread libHalf libexpat liblcms libintl libgettextsrc-$GETTEXT_VERSION libgettextpo libgettextlib-$GETTEXT_VERSION libasprintf libexiv2 libGLEW libxmi libiconv" 
+libs="libwx_macu-$WX_MAJOR_VERSION libwx_macu_gl-$WX_MAJOR_VERSION libpano13 $BOOST_THREAD_LIB-$BOOST_VER $BOOST_DATE_TIME_LIB-$BOOST_VER $BOOST_FILESYSTEM_LIB-$BOOST_VER $BOOST_IOSTREAMS_LIB-$BOOST_VER $BOOST_REGEX_LIB-$BOOST_VER $BOOST_SYSTEM_LIB-$BOOST_VER $BOOST_SIGNALS_LIB-$BOOST_VER libpng libtiff libtiffxx libjpeg libIex libImath libIlmImf libIlmThread libHalf libexpat liblcms libintl libgettextsrc-$GETTEXT_VERSION libgettextpo libgettextlib-$GETTEXT_VERSION libasprintf libexiv2 libGLEW libxmi libiconv" 
 
 #binaries="$App/Contents/$dylib_install_loc/*.dylib $App/Contents/MacOS/* $App/Contents/Frameworks/Hugin*.framework/Hugin* $App/Contents/Frameworks/icpfind.framework/icpfind* $App/Contents/Frameworks/local*.framework/local* $App/Contents/Frameworks/hugin_python*.framework/hugin* $App/Contents/Resources/align_image_stack $App/Contents/Resources/cpfind"
 # temporary remove python stuff
@@ -49,3 +49,14 @@ do
  done
 
 done
+
+
+# fix issue with libboost libraries where the symlink is not copied
+cd $TARGET_BUILD_DIR/$PRODUCT_NAME.app/Contents/Libraries
+ln -s libboost_date_time-$BOOST_VER.dylib libboost_date_time.dylib
+ln -s libboost_filesystem-$BOOST_VER.dylib libboost_filesystem.dylib
+ln -s libboost_iostreams-$BOOST_VER.dylib libboost_iostreams.dylib
+ln -s libboost_regex-$BOOST_VER.dylib libboost_regex.dylib
+ln -s libboost_thread-$BOOST_VER.dylib libboost_thread.dylib
+ln -s libboost_system-$BOOST_VER.dylib libboost_system.dylib
+
