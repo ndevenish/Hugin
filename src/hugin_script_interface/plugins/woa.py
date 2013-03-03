33032eda8e28f1f31fa6424eaae4627d4d9c0152
harryvanderwolf <hvdwolf@gmail.com>
2013-03-03 15:28:49 +0100

    bumping version number

--- src/hugin_script_interface/plugins/woa.py
+++ src/hugin_script_interface/plugins/woa.py
@@ -25,7 +25,7 @@ gpl = r"""
 # @category Control Points
 # @name     Warped Overlap Analysis
 # @api-min  2011.1
-# @api-max  2012.2
+# @api-max  2013.1
 
 # note that if you want to read the script, it's written bottom-up, so the
 # higher-level routines are towards the end.
