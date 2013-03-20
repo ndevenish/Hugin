c5103f5d6a1a75b581d53cd80b8fc41c533983cd
tmodes <devnull@localhost>
2013-03-20 18:44:59 +0100

    Hide panorama editor when switching to simple interface

--- src/hugin1/hugin/MainFrame.cpp
+++ src/hugin1/hugin/MainFrame.cpp
@@ -2102,6 +2102,7 @@ void MainFrame::SetGuiLevel(GuiLevel newLevel)
             gl_preview_frame->SetTitle(scriptName.GetName() + wxT(".") + scriptName.GetExt() + wxT(" - ") + _("Hugin - Panorama Stitcher"));
             SetTitle(scriptName.GetName() + wxT(".") + scriptName.GetExt() + wxT(" - ") + _("Panorama editor"));
         };
+        Hide();
     }
     else
     {
