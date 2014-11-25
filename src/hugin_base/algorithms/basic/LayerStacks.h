15cc003ed2193171f019cc0656cab234ca721442
tmodes <devnull@localhost>
2014-11-25 19:50:28 +0100

    Refactored code

--- src/hugin_base/algorithms/basic/LayerStacks.h
+++ src/hugin_base/algorithms/basic/LayerStacks.h
@@ -29,10 +29,10 @@
 namespace HuginBase
 {
 /** returns vector of set of output stacks */
-IMPEX std::vector<UIntSet> getHDRStacks(const PanoramaData & pano, UIntSet allImgs, PanoramaOptions opts);
+IMPEX UIntSetVector getHDRStacks(const PanoramaData & pano, UIntSet allImgs, PanoramaOptions opts);
 /** returns vector of set of output exposure layers */
-IMPEX std::vector<UIntSet> getExposureLayers(const PanoramaData & pano, UIntSet allImgs, PanoramaOptions opts);
-IMPEX std::vector<UIntSet> getExposureLayers(const PanoramaData & pano, UIntSet allImgs, const double maxEVDiff);
+IMPEX UIntSetVector getExposureLayers(const PanoramaData & pano, UIntSet allImgs, PanoramaOptions opts);
+IMPEX UIntSetVector getExposureLayers(const PanoramaData & pano, UIntSet allImgs, const double maxEVDiff);
 /** returns set of images which are visible in output ROI */
 IMPEX UIntSet getImagesinROI(const PanoramaData& pano, const UIntSet activeImages);
 /** returns set of images which are visible in given ROI */
