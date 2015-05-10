b345d9ddd0e167f502705e7a50da0c95fda9c554
tmodes <devnull@localhost>
2015-05-10 09:01:38 +0200

    Update FSF address and use copyright header currently suggested by the FSF with hyperlink instead of FSF postal address. (Patch by Andreas Metzler) [1453385]

--- src/foreign/getopt/src/getopt.c
+++ src/foreign/getopt/src/getopt.c
@@ -17,8 +17,7 @@
    GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
+   along with this program.  If not, see <http://www.gnu.org/licenses/> */
 
 /* NOTE!!!  AIX requires this to be the first thing in the file.
    Do not put ANYTHING before it!  */
