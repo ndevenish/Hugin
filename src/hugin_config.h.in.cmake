220b51278ac56c723fa552e466aa9cc99b96b82d
tmodes <devnull@localhost>
2016-03-28 18:46:51 +0200

    Clean up hugin_config.h.
    Removed unused parameters

--- src/hugin_config.h.in.cmake
+++ src/hugin_config.h.in.cmake
@@ -1,46 +1,5 @@
 #ifndef __CONFIG_H__
 
-/* various libraries. For compatability with the old source code
- * most cmake variables are defined under a second name as well */
-
-/* Define if you have JPEG library */
-#cmakedefine JPEG_FOUND 1
-/* Define if you have JPEG library (old style) */
-#ifdef JPEG_FOUND
-#define HasJPEG 1
-#endif
-
-/* Define if you have PNG library */
-#cmakedefine PNG_FOUND 1
-#ifdef PNG_FOUND
-#define HasPNG 1
-#endif
-
-/* Define if you have TIFF library */
-#cmakedefine TIFF_FOUND 1
-#ifdef TIFF_FOUND
-#define HasTIFF 1
-#endif
-
-/* Define if you have OpenEXR library */
-#cmakedefine OPENEXR_FOUND 1
-#ifdef OPENEXR_FOUND
-#define HasEXR 1
-#endif
-
-/* Define if you have Panotools library (pano13) */
-#cmakedefine TLALLI_FOUND 1
-#cmakedefine PANO13_FOUND 1
-
-#ifdef TLALLI_FOUND
-#define HasTLALLI 1
-#elif defined PANO13_FOUND
-#define HasPANO13 1
-#endif
-
-/* Define if you have log2 function */
-#cmakedefine HAVE_LOG2 1
-
 /* Define if you have log1p function */
 #cmakedefine HAVE_LOG1P 1
 
